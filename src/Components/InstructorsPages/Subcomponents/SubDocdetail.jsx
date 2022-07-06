196650fae42a7f28314f3a86681711960c9b802f
moemyint <moemyintzaw.technology@gmail.com>
2022-07-06 14:09:52 +0630

    modified docdetail
    text modified

--- src/Components/InstructorsPages/Subcomponents/SubDocdetail.jsx
+++ src/Components/InstructorsPages/Subcomponents/SubDocdetail.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import image from '../../../Images/drdisrespect.webp'
 import * as MUI from '@mui/material'
+import "../Styledetails.scss"
 
 const SubDocdetail = () => {
   return (
@@ -11,6 +12,17 @@ const SubDocdetail = () => {
             </MUI.Box>
             <MUI.Box className='D-box'>
                 <MUI.Typography className='D-text' variant='h5'>D</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>R</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>.D</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>I</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>S</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>R</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>E</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>S</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>P</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>E</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>C</MUI.Typography>
+                <MUI.Typography className='D-text' variant='h5'>T</MUI.Typography>
             </MUI.Box>
        </MUI.Box>
     </>
